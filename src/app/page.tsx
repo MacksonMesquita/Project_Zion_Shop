fcb64616fd3912401eebbe35a4c2fdbdc2698e2b
Mackson Mesquita <macksondar.mesquita17@gmail.com>
2023-11-29 22:34:20 -0300

    :sparkles: New components and features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import UserHoverCard from "@/components/Avatar";
-import { NavBar } from "@/components/NavBar/inndex";
+import { NavBar } from "@/components/NavBar";
 import { Title } from "@/components/Title";
 
 export default function Home() {
